d72d9d24a56cdd580733db4ea74f87a1391d5889
François Briatte <briatte@gmail.com>
2021-07-20 15:05:24 +0200

    download authors info separately from sessions/abstracts
    better identification of names, authors, and who presented what

--- 01-download.py
+++ 01-download.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# # Download EPSA 2019 Web pages
+# # Download EPSA 2019 sessions and abstracts
 
 # In[1]:
 
@@ -13,17 +13,26 @@ import re
 import time
 
 
-# # Get panel URLs
-
-# The `html/presentation-types.html` page was manually downloaded from the [conference website](https://app.oxfordabstracts.com/events/772/), using an Oxford Abstracts user account.
+# ## Create download folders
 
 # In[2]:
+
+
+os.makedirs("html/sessions", exist_ok = True)
+os.makedirs("html/abstracts", exist_ok = True)
+
+
+# ## Find panel URLs
+
+# The `html/presentation-types.html` page was [manually downloaded](https://app.oxfordabstracts.com/events/772/program-app/presentation-types) from the [conference website](https://app.oxfordabstracts.com/events/772/).
+
+# In[3]:
 
 
 panel_page = BeautifulSoup(open("html/presentation-types.html"), "lxml")
 
 
-# In[3]:
+# In[4]:
 
 
 panel_urls = []
@@ -31,23 +40,21 @@ for i in panel_page.find_all("a", href = re.compile("session")):
     panel_urls.append(i.get("href"))
 
 
-# In[4]:
+# In[5]:
 
 
 panel_urls[0:4]
 
 
-# In[5]:
-
-
-len(panel_urls)
-
-
 # In[6]:
 
 
-# unique values
-panel_urls = list(set(panel_urls))
+# check length of panel id is always 4
+j = []
+for i in panel_urls:
+    j.append(len(re.search(r'\d+$', i).group(0)))
+
+assert set(j) == {4}, "Unusual panel ids detected."
 
 
 # In[7]:
@@ -59,24 +66,37 @@ len(panel_urls)
 # In[8]:
 
 
+# unique values
+panel_urls = list(set(panel_urls))
+
+
+# In[9]:
+
+
+len(panel_urls)
+
+
+# In[10]:
+
+
 panel_urls[0:4]
 
 
-# # Download panels
+# ## Download panels
 
-# ## Warning: empty pages
+# ### Warning: empty pages
 
 # Some pages will fail to load properly despite the time allocated for that, and will contain nothing except a "Loading event data" message.
 # 
 # Delete them and run the notebook again.
 
-# In[9]:
+# In[11]:
 
 
 driver = webdriver.Chrome('/Applications/chromedriver')
 
 
-# In[10]:
+# In[12]:
 
 
 left = panel_urls[::-1]
@@ -84,7 +104,7 @@ left = panel_urls[::-1]
 for i in panel_urls:
     u = "https://app.oxfordabstracts.com" + i
     f = "html/sessions/session" + i[-4:] + ".html"
-    print("(" + str(left.index(i) + 1) + " left) URL: " + u)
+    print("(" + str(left.index(i) + 1).rjust(3) + " left) " + u) #  + " -> " + f
     if os.path.exists(f):
         next
     else:
@@ -97,11 +117,11 @@ for i in panel_urls:
 print("done")
 
 
-# # Get abstracts URLs
+# ## Find abstracts URLs
 
 # This will fail if the page loaded improperly. In that case, go back to previous step and download the page again.
 
-# In[11]:
+# In[13]:
 
 
 files = [f for f in os.listdir("html/sessions") if re.match(r'.*\.html', f)]
@@ -115,32 +135,33 @@ for i in files:
     z = []
     for j in x.find_all("a", href = re.compile("submission")):
         z.append(j.get("href"))
-    print(i + ": " + y.replace('Presentation type', '') + " with " + str(len(z)) + " abstracts")
+    print(i + ": " + y.replace('Presentation type', '') + " with " + str(len(z)) + " abstract(s)")
     abstract_urls.append(z)
 
 
-# In[12]:
+# In[14]:
 
 
 # should equal number of panels
-len(abstract_urls)
+assert len(panel_urls) == len(panel_urls), "Some session pages were not parsed."
 
 
-# In[13]:
+# In[15]:
 
 
+# unlist
 abstract_urls = [item for sublist in abstract_urls for item in sublist]
 abstract_urls[0:4]
 
 
-# In[14]:
+# In[16]:
 
 
 # actual number of abstracts
 len(abstract_urls)
 
 
-# In[15]:
+# In[17]:
 
 
 # unique values
@@ -148,23 +169,38 @@ abstract_urls = list(set(abstract_urls))
 len(abstract_urls)
 
 
-# In[16]:
+# In[18]:
 
 
 abstract_urls[0:4]
 
 
-# # Download abstracts
+# ### Warning: abstract ids
 
-# In[17]:
+# Abstract identifier length is actually variable.
+
+# In[19]:
+
+
+# check length of abstract id is always 5 or 6
+j = []
+for i in abstract_urls:
+    j.append(len(re.search(r'\d+$', i).group(0)))
+
+assert set(j) == {5, 6}, "Unusual abstract ids detected."
+
+
+# ## Download abstracts
+
+# In[20]:
 
 
 left = abstract_urls[::-1]
 
 for i in abstract_urls:
     u = "https://app.oxfordabstracts.com" + i
-    f = "html/abstracts/abstract_" + i[-4:] + ".html"
-    print("(" + str(left.index(i) + 1) + " left) URL: " + u)
+    f = "html/abstracts/abstract_" + re.search(r'\d+$', i).group(0) + ".html"
+    print("(" + str(left.index(i) + 1).rjust(3) + " left) " + u) #  + " -> " + f
     if os.path.exists(f):
         next
     else:
@@ -176,3 +212,5 @@ for i in abstract_urls:
 
 print("done")
 
+
+# Have a nice day.
